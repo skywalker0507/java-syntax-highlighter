69ec77e32f066b73c65bef8053ebd1095d6b8e6c
hski <holger@schimanski-web.de>
2014-07-01 21:15:12 +0200

    Revert back to testing noting

--- test/syntaxhighlighter/SyntaxHighlighterTest.java
+++ test/syntaxhighlighter/SyntaxHighlighterTest.java
@@ -1,15 +1,11 @@
 package syntaxhighlighter;
 
-import static org.junit.Assert.assertTrue;
-
-import org.jdom2.Element;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import syntaxhighlighter.brush.BrushJava;
+import static org.junit.Assert.*;
 
 /**
  * @author Chan Wai Shing <cws1989@gmail.com>
@@ -40,28 +36,5 @@ public class SyntaxHighlighterTest {
     // there is no testing for brushes in original JavaScript version of SyntaxHighlighter and so far there is no plan for me to create some myself.
     // if you want something more reliable, you can choose Java Prettify (http://java-prettify.googlecode.com), which has much more tests.
     assertTrue(true);
-    
-    String aJavaString = 
-    		"/* Some multiline comment\n" + 
-    		" * which span over \n" +
-    		" */\n" + 
-    		"   Some multi line code without any\n" +
-    		"   matching word or text\n" +
-    		"String someString = \"An example comment: /* example */\";\n" +
-			"// The comment around this code has been commented out.\n" +
-			"// /*\n" +
-			"some_code(\"an example string\", true);\n" +
-			"// */\";\n" +
-			"some text at \n" +
-			"the end";
-    
-    System.out.println(aJavaString);
-    
-    Element container = SyntaxHighlighterHtmlUtil.brush(aJavaString, new BrushJava());
-    Element output = SyntaxHighlighterHtmlUtil.addGutterAndTitle(container, "Hello.java", 5, false);
-    
-    System.out.println(SyntaxHighlighterHtmlUtil.outputString(output));
-
-    
   }
 }
